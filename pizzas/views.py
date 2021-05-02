6c8c39d4f3b3664e85874ad7bf58bdf3a768fbac
Gibram Favela <Gibram_Favela1@baylor.edu>
2021-05-02 17:43:20 -0500

    added pizza form and more urls

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Pizza
+from .forms import PizzaForm
 
 # Create your views here.
 
@@ -16,3 +17,33 @@ def pizzas(request):
     context = {'pizzas':pizzas}
 
     return render(request, 'pizzas/pizzas.html', context)
+
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+
+    toppings = pizza.topping_set.order_by('-date_added')
+    comments = pizza.comment_set.order_by('-date_added')
+
+    context = {'pizza': pizza, 'toppings': toppings, 'comments': comments}
+
+    return render(request, 'pizzas/pizza.html', context)
+
+
+def new_pizza(request):
+    if request.method != 'POST':
+        form = PizzaForm()
+
+    else:
+        form = PizzaForm(data=request.POST)
+
+        if form.is_valid():
+
+            new_pizza = form.save(commit=False)
+
+            new_pizza.save()
+
+            return redirect('pizzas:pizza')
+
+    context = {'form': form}
+    return render(request, 'pizzas/new_pizza.html', context)
