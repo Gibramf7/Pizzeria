821c1909838432fd7b76cd7a996e85dd99fe05e2
Gibram Favela <Gibram_Favela1@baylor.edu>
2021-05-02 04:36:20 -0500

    initial commit

---
+++ MyShell.py
@@ -0,0 +1,27 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizzeria.settings")
+
+import django
+django.setup()
+
+from pizzas.models import Pizza, Topping
+
+pizzas = Pizza.objects.all()
+
+
+for pizza in pizzas:
+    print(pizza.id, pizza)
+
+toppings = Topping.objects.all()
+for t in toppings:
+    print(f"Pizza: {t.pizza}")
+    print(f"Name: {t}")
+    print(f"Date Added: {t.date_added}")
+
+
+'''
+toppings = t.topping_set.all()
+
+for topping in toppings:
+    print(topping)
+'''
