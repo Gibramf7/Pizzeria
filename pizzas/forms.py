07838f10594df0542053d46ffc6a1c403eceeffc
Gibram Favela <Gibram_Favela1@baylor.edu>
2021-05-03 15:57:58 -0500

    added comments

--- pizzas/forms.py
+++ pizzas/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Pizza, Topping
+from .models import Pizza, Topping, Comment
 
 
 class PizzaForm(forms.ModelForm):
@@ -16,4 +16,8 @@ class ToppingForm(forms.ModelForm):
         labels = {'name': ''}
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
 
-
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name']
+        labels = {'name': ''}
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
-from .models import Pizza, Topping
-from .forms import PizzaForm, ToppingForm
+from .models import Pizza, Topping, Comment
+from .forms import PizzaForm, ToppingForm, CommentForm
 
 # Create your views here.
 
@@ -90,3 +90,24 @@ def edit_topping(request, topping_id):
     context = {'topping': topping, 'pizza': pizza, 'form': form}
 
     return render(request, 'pizzas/edit_topping.html', context)
+
+
+def new_comment(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+
+    if request.method != 'POST':
+        form = CommentForm()
+
+    else:
+        form = CommentForm(data=request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.pizza = pizza
+            new_comment.save()
+
+            return redirect('pizzas:pizza', pizza_id=pizza_id)
+
+    context = {'form': form, 'pizza': pizza}
+
+    return render(request, 'pizzas/new_comment.html', context)
